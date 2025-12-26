6b2ff25193c77c534df294ee3ca78d1c08be1812
Pujesh-Kumar-Yadav <yadavpujesh028@gmail.com>
2025-12-26 22:34:58 +0545

    first commit

--- javascript.js/task.js
+++ javascript.js/task.js
@@ -660,6 +660,14 @@
 //   if()
 // }
 
+for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        console.log(arr1[i]);
+      }
+    }
+  }
+
 
 
 
